1ddc3309686a380d126de58cb116af802c295afd
hanhnguyen582001 <tt.hanh.nguyen@tomosia.com>
2023-03-10 17:08:24 +0700

    chapter 3,chapter 4

--- ruby_on_rails/sample_app/config/routes.rb
+++ ruby_on_rails/sample_app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/bar'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#hello"
+  root 'static_pages#home'
 end
