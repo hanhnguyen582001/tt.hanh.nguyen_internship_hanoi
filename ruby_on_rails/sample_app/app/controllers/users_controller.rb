b5a374a20cb9115fdd346fcdff2f67b426e5cbee
hanhnguyen582001 <tt.hanh.nguyen@tomosia.com>
2023-03-16 17:40:08 +0700

    chapter 8

--- ruby_on_rails/sample_app/app/controllers/users_controller.rb
+++ ruby_on_rails/sample_app/app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    debugger
     @user = User.find(params[:id])
   end
 
--- ruby_on_rails/sample_app/config/routes.rb
+++ ruby_on_rails/sample_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   get "/signup", to: "users#new"
   get "/home", to: "static_pages#home"
   # get "static_pages/home"
@@ -6,6 +7,9 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   resources :users
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
