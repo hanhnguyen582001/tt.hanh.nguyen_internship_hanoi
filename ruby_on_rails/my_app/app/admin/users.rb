acc36f5ac91112f8f361405acfc9ea12cc56bc0c
hanhnguyen582001 <tt.hanh.nguyen1@tomosia.com>
2023-04-17 18:03:43 +0700

    custom active admin

--- ruby_on_rails/my_app/app/admin/users.rb
+++ ruby_on_rails/my_app/app/admin/users.rb
@@ -1,24 +1,55 @@
 ActiveAdmin.register User do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
+
   permit_params :name, :email, :password, :password_confirmation, :admin
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :email, :password_digest, :remember_digest, :admin, :activation_digest, :activated, :activated_at, :reset_digest, :reset_sent_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
   menu label: proc { I18n.t 'active_admin.user' }
+  menu priority: 1
+
+  filter :email
+  filter :name, label: I18n.t('active_admin.user_page.label.name')
+  filter :admin, label: I18n.t('active_admin.user_page.label.role'), as: :select, collection: [[I18n.t('active_admin.admin_user'), 1], [I18n.t('active_admin.user'), 0]]
+  filter :following, label: I18n.t('active_admin.user_page.label.following')
+  filter :followers, label: I18n.t('active_admin.user_page.label.follower')
+  filter :created_at
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :name
+    column :created_at
+    actions do |user|
+      link_to I18n.t('active_admin.action.microposts'), admin_microposts_path(q: { user_id_eq: user.id }), class: "view_link member_link c-button c-button--info u-xsmall"
+    end
+  end
+
+  show title: :name do
+    attributes_table do
+      row :name
+      row :email
+    end
+    panel "User microposts" do
+      table_for(user.microposts) do
+        column("microposts", sortable: :id) do |micropost|
+          link_to micropost.id.to_s, admin_micropost_path(micropost)
+        end
+        column("content") { |micropost| micropost.content }
+        column("Create date", sortable: :created_at) do |micropost|
+          pretty_format(micropost.created_at)
+        end
+      end
+    end
+
+    active_admin_comments
+  end
+
   form do |f|
+    f.semantic_errors
     f.inputs do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :admin, as: :check_boxes, collection: [[I18n.t('active_admin.admin_user'), 1]], label: false
     end
     f.actions
   end
--- ruby_on_rails/my_app/app/models/user.rb
+++ ruby_on_rails/my_app/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
   # after_validation { puts 2 }
